25978042c4f9ee53206a1a0d8c76dbc9761cd872
brookesDaphne <brookesDaphne123@gmail.com>
2016-03-17 16:26:36 -0400

    nav

---
+++ js/getNav.js
@@ -0,0 +1,12 @@
+$.get("http://brookesDaphne.github.io/partials/nav.html", function(data) {
+
+  $(document).ready(function() {
+	//check if this document has .container
+	var container = $(".container");
+	//if so add nav.html contents to the top
+	if (container) {
+  	container.prepend(data)
+  	//container.fadeIn();
+	}
+  });
+});
--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -12,24 +12,24 @@ $(document).ready(function() {
         //<tr> <td></td> <td></td> </tr>
         //<table>
 
-        var html = "<table class='table table-hover table-striped'>"+
+        var html = "<table class='table table-hover table-striped'>" +
           "<tr> <th>Name</th> <th>Email</th> <th>Company</th> </tr>";
 
         $.each(data, function(index, item) {
 
           //$("#data").append(item.name);
 
-html+= "<tr>"+
-"<td>" + item.name + "</td>" +
-"<td>" + item.email + "</td>" +
-"<td>"+item.company + "</td>" +
-"</tr>";
+          html += "<tr>" +
+            "<td>" + item.name + "</td>" +
+            "<td>" + item.email + "</td>" +
+            "<td>" + item.company + "</td>" +
+            "</tr>";
 
         })
 
         html += "</table>";
 
-//puts all html into the div
+        //puts all html into the div
         $("#data").append(html);
 
         //alert(data;)
