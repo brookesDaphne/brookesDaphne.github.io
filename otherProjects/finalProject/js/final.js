b9a7a20a132eba1386dad07fe4df3fb1f6315114
brookesDaphne <brookesDaphne123@gmail.com>
2016-03-29 09:38:41 -0400

    more final

--- otherProjects/finalProject/js/final.js
+++ otherProjects/finalProject/js/final.js
@@ -1,24 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  //to change the nav depending on what page is being viewed
+      //to change the nav depending on what page is being viewed
 
-//get all the nav li, add click event
-  $(".nav").find("li").on("click",function(){
+      //get all the nav li, add click event
+      $(".nav").find("li").on("click", function() {
 
-//remove all active class
-$(".nav").find("li").removeClass("active");
+        //remove all active class
+        $(".nav").find("li").removeClass("active");
 
-//add active class to clicked li in nav
-$(this).addClass("active");
-
-
-
-var page = $(this).attr("id");
-alert(page);
-
-  }) //end brackets for click
+        //add active class to clicked li in nav
+        $(this).addClass("active");
 
 
 
+        var page = $(this).attr("id");
+        getPartial(page);
+      })
 
-}) //end brackets for ready
+      function getPartial(partial) {
+
+        if (partial == "homePage") { //ajax get home.html
+          $.get("partials/home.html", function(data) {
+            $("#pageContent").html(data);
+
+          })
+        } else if (partial == "models") { //ajax get models.html
+          alert("2");
+
+
+        } else if (partial == "order") { //ajax get order.html
+          alert("3");
+
+        }
+
+
+
+        //begin the program, get the homepage
+        getPartial("homePage");
+
+
+      }) //end brackets for ready
